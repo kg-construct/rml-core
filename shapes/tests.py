f7ca1340dc5f355255bb4b56a3c3f3095e0e9977
David Chaves <david.chaves@usc.es>
2025-09-27 09:55:38 +0200

    switching ignored test-case

--- shapes/tests.py
+++ shapes/tests.py
@@ -47,7 +47,7 @@ class MappingValidatorTests(unittest.TestCase):
         print(f'Testing validation with: {path}')
         if 'RMLTC0004b' in path or 'RMLTC0007h' in path or \
                 'RMLTC0012c' in path or 'RMLTC0012d' in path or \
-                'RMLTC0015b' in path or 'RMLTC0023d' in path: 
+                'RMLTC0015b' in path or 'RMLTC0023c' in path:
             with self.assertRaises(Exception):
                 self._validate_rules(path)
         else:
